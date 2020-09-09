8d16d1ebc7af604e47daeab56ff0acf2b3379108
Ninorp <rodolfo.520@live.com>
2020-09-09 18:46:18 -0300

    adicionando outros resources

--- src/app/api.py
+++ src/app/api.py
@@ -1,7 +1,7 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from tastypie import fields
-from .models import Book, User, Author
+from .models import Book, User, Author, Publisher, Client, Sale
 
 
 class UserResource(ModelResource):
@@ -16,6 +16,38 @@ class BookResource(ModelResource):
     class Meta:
         queryset = Book.objects.all()
         resource_name = 'book'
+        authorization = Authorization()
+
+
+class AuthorResource(ModelResource):
+
+    class Meta:
+        queryset = Author.objects.all()
+        resource_name = 'author'
+        authorization = Authorization()
+
+
+class PublisherResource(ModelResource):
+
+    class Meta:
+        queryset = Publisher.objects.all()
+        resource_name = 'publisher'
+        authorization = Authorization()
+
+
+class ClientResource(ModelResource):
+
+    class Meta:
+        queryset = Client.objects.all()
+        resource_name = 'client'
+        authorization = Authorization()
+
+
+class SaleResource(ModelResource):
+
+    class Meta:
+        queryset = Sale.objects.all()
+        resource_name = 'author'
         authorization = Authorization()
 
 
